28ff99f274563c424e5b8479436e535902d994af
Wouter van Atteveldt <wouter@vanatteveldt.com>
2022-07-05 19:12:38 +0200

    Update amcat4react version and add 'add field' option

--- src/components/Pages/Fields.tsx
+++ src/components/Pages/Fields.tsx
@@ -1,16 +1,41 @@
+import { Amcat, AmcatField } from "amcat4react";
 import {
   getField,
   getFieldTypeIcon,
   useFieldsWithRefresh,
 } from "amcat4react/dist/Amcat";
-import { Checkbox, Header, Icon, Table } from "semantic-ui-react";
+import { useState } from "react";
+import { Checkbox, Form, Header, Icon, Table } from "semantic-ui-react";
 import { setField } from "../../lib/login";
 import { useIndex } from "../../lib/navigation";
+
+const FIELDS = [
+  "date",
+  "keyword",
+  "long",
+  "tag",
+  "text",
+  "url",
+  "double",
+  "id",
+  "object",
+  "geo_point",
+];
 
 export default function Fields() {
   const index = useIndex();
   const [fields, refresh] = useFieldsWithRefresh(index);
+  const [newFieldName, setNewFieldName] = useState("");
+  const [newFieldType, setNewFieldType] = useState("keyword");
+  const [newFieldError, setNewFieldError] = useState<string>();
+
   if (index == null || fields == null) return null;
+  const fieldOptions = FIELDS.map((f) => ({
+    value: f,
+    key: f,
+    text: f,
+    icon: getFieldTypeIcon(f),
+  }));
 
   const set_field_meta = (key: string, field: string, value?: boolean) => {
     const f = getField(fields, field);
@@ -21,12 +46,30 @@ export default function Fields() {
     console.log(type);
     setField(index, field, type)
       .then((e) => {
-        console.log("OK, refreshing fields!");
         refresh();
       })
       .catch((e) => console.error(e));
   };
-  console.log(fields);
+
+  function validateFieldName(name: string) {
+    if (newFieldName.trim() === "") return "Please enter a field name";
+    return null;
+  }
+
+  function addField() {
+    const error = validateFieldName(newFieldName);
+    if (error != null) {
+      setNewFieldError(error);
+      return;
+    }
+    setNewFieldError(undefined);
+    if (index == null) return;
+    const f = { name: newFieldName, type: newFieldType } as AmcatField;
+    Amcat.addFields(index, [f]).then(() => {
+      refresh();
+    });
+  }
+
   return (
     <>
       <Header>Fields used in {index.index}</Header>
@@ -71,6 +114,30 @@ export default function Fields() {
           ))}
         </Table.Body>
       </Table>
+      <Form>
+        <Form.Group widths="equal">
+          <Form.Input
+            fluid
+            error={newFieldError}
+            label="Field name"
+            placeholder="Field name"
+            value={newFieldName}
+            onChange={(_, d) => setNewFieldName(d.value as string)}
+          />
+          <Form.Dropdown
+            fluid
+            selection
+            label="Field type"
+            value={newFieldType}
+            options={fieldOptions}
+            onChange={(_, d) => setNewFieldType(d.value as string)}
+          />
+          <Form.Button onClick={() => addField()} positive label="&nbsp;">
+            <Icon name="add" />
+            Add field
+          </Form.Button>
+        </Form.Group>
+      </Form>
     </>
   );
 }
