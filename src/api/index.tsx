fb93d93c33682a972769004ee3955bf5150e407a
Kasper Welbers <kasperwelbers@gmail.com>
2024-01-16 17:19:24 +0100

    adding create/delete field

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,9 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { amcatUserRoles, amcatIndexSchema } from "@/schemas";
 import { AmcatIndexName, AmcatUserRole } from "@/interfaces";
 import { useHasGlobalRole } from "./userDetails";
 import { useAmcatConfig } from "./config";
 import { MiddlecatUser } from "middlecat-react";
+import { toast } from "sonner";
 
 export function useIndex(user?: MiddlecatUser, indexName?: AmcatIndexName) {
   return useQuery({
@@ -35,4 +36,53 @@ async function getIndex(user?: MiddlecatUser, indexName?: string) {
   const res = await user.api.get(`/index/${indexName}`);
   console.log(res.data);
   return amcatIndexSchema.parse(res.data);
+}
+
+interface MutateIndexParams {
+  id: AmcatIndexName;
+  action: "create" | "delete" | "update";
+  name?: string;
+  description?: string;
+  summary_field?: string;
+  guest_role?: AmcatUserRole;
+}
+
+export function useMutateIndex(user: MiddlecatUser, indexName: AmcatIndexName) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ id, name, description, summary_field, guest_role, action }: MutateIndexParams) => {
+      if (!user) throw new Error("Not logged in");
+      return mutateIndx(user, indexName, action, name, description, summary_field, guest_role);
+    },
+    onSuccess: (data, variables) => {
+      queryClient.invalidateQueries({ queryKey: ["index", user, indexName] });
+      queryClient.invalidateQueries({ queryKey: ["indices", user] });
+
+      if (variables.action === "create") toast.success(`Created index ${variables.id}`);
+      if (variables.action === "update") toast.success(`Updated index ${variables.id}`);
+      if (variables.action === "delete") toast.success(`Deleted index ${variables.id}`);
+    },
+    onError: (error: any, variables) => {
+      toast(`Could not ${variables.action} user: `, { description: error?.response?.data?.detail || error.message });
+    },
+  });
+}
+
+export async function mutateIndx(
+  user: MiddlecatUser,
+  id: AmcatIndexName,
+  action: "create" | "delete" | "update",
+  name?: string,
+  description?: string,
+  summary_field?: string,
+  guest_role?: AmcatUserRole,
+) {
+  if (action === "delete") {
+    return await user.api.delete(`index/${id}`);
+  } else if (action === "create") {
+    return await user.api.post(`index`, { id, name, description, summary_field, guest_role });
+  } else if (action === "update") {
+    return await user.api.put(`index/${id}`, { name, description, summary_field, guest_role });
+  }
 }
