e7b38ed0743a236f25e22bcb1351a5e7c912749f
Kasper Welbers <kasperwelbers@gmail.com>
2024-03-01 18:35:38 +0100

    ideas for fool proof visualizations

--- src/components/Aggregate/AggregateLineChart.tsx
+++ src/components/Aggregate/AggregateLineChart.tsx
@@ -4,13 +4,35 @@ import { CustomTooltip } from "./CustomTooltip";
 import useCreateChartData from "./useCreateChartData";
 import { qualitativeColors } from "./colors";
 import { Loading } from "../ui/loading";
-import React, { unstable_ConcurrentMode as ConcurrentMode } from "react";
+import { useMemo, useState } from "react";
+import { ArrowBigRight, ArrowLeft, ArrowRight } from "lucide-react";
+import { Button } from "../ui/button";
 
 export default function AggregateLineChart({ data, onClick, width, height, limit }: AggregateVisualizerProps) {
   const [chartData, status] = useCreateChartData(data, true);
-  console.log(chartData, status);
+  const [page, setPage] = useState(0);
+  const pageSize = 250;
+
+  const maxValue = useMemo(() => {
+    let max = 0;
+    if (!chartData) return max;
+    for (let row of chartData.d) {
+      for (let column of chartData.columns) {
+        max = Math.max(max, row[column]);
+      }
+    }
+    return max;
+  }, [chartData]);
+  const showData = useMemo(() => {
+    if (!chartData) return null;
+    const start = page * pageSize;
+    const end = start + pageSize;
+    return chartData.d.slice(start, end);
+  }, [page, chartData, pageSize]);
+
   if (status === "loading") return <Loading />;
-  if (!chartData) return null;
+  if (!showData || !chartData) return null;
+
   const colors = qualitativeColors(chartData.columns.length);
 
   const handleClick = (line: number, point: any) => {
@@ -24,35 +46,49 @@ export default function AggregateLineChart({ data, onClick, width, height, limit
     onClick(values);
   };
 
-  if (limit && chartData.columns.length > limit) {
-    const counts = Object.fromEntries(chartData.columns.map((c) => [c, 0]));
-    const f = data.meta.axes[1].field;
-    data.data.forEach((row) => (counts[row[f]] += row.n));
-    chartData.columns = Object.keys(counts)
-      .sort((k1, k2) => counts[k2] - counts[k1])
-      .slice(0, limit);
-  }
-
+  const hasPagination = showData.length < chartData.d.length;
   if (height == null) height = 300;
   if (width == null) width = "100%";
   return (
-    <ResponsiveContainer height={height} width={width} className="text-sm">
-      <LineChart data={chartData.d}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey={data.meta.axes[0].name} />
-        <YAxis />
-        <Tooltip content={<CustomTooltip />} />
-        {chartData.columns.length > 1 ? <Legend /> : null}
-        {chartData.columns.map((column, i) => (
-          <Line
-            key={column + i}
-            type="monotone"
-            dataKey={column}
-            stroke={colors[i]}
-            activeDot={{ onClick: (e, j) => handleClick(i, j) }}
-          />
-        ))}
-      </LineChart>
-    </ResponsiveContainer>
+    <div>
+      <ResponsiveContainer height={height} width={width} className="text-sm">
+        <LineChart data={showData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey={data.meta.axes[0].name} />
+          <YAxis domain={[0, maxValue]} />
+          <Tooltip content={<CustomTooltip />} />
+          {chartData.columns.length > 1 ? <Legend /> : null}
+          {chartData.columns.map((column, i) => (
+            <Line
+              key={column + i}
+              type="monotone"
+              dataKey={column}
+              stroke={colors[i]}
+              activeDot={{ onClick: (e, j) => handleClick(i, j) }}
+            />
+          ))}
+        </LineChart>
+      </ResponsiveContainer>
+      <div
+        className={`ml-16 grid select-none grid-cols-[1fr,1fr,1fr] items-center ${hasPagination ? "block" : "hidden"}`}
+      >
+        <Button variant="ghost" className="flex gap-3" onClick={() => setPage(page - 1)} disabled={page === 0}>
+          <ArrowLeft />
+          Previous
+        </Button>
+        <div className="mx-auto px-3">
+          {page + 1} of {Math.ceil(chartData.d.length / pageSize)}
+        </div>
+        <Button
+          variant="ghost"
+          className="flex gap-3"
+          onClick={() => setPage(page + 1)}
+          disabled={showData.length < pageSize}
+        >
+          Next
+          <ArrowRight />
+        </Button>
+      </div>
+    </div>
   );
 }
