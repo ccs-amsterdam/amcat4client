0da48535f95d176cb0f4d30f9d695b1b94e6a603
Wouter van Atteveldt <wouter@vanatteveldt.com>
2024-04-07 13:20:07 +1000

    Add link to field settings if no dashboard visualizations present

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -7,6 +7,7 @@ import { useMemo } from "react";
 import { useFieldValues } from "@/api/fieldValues";
 import { useFieldStats } from "@/api/fieldStats";
 import { autoFormatDate } from "@/lib/autoFormatDate";
+import { useHasIndexRole } from "@/api";
 
 interface Props {
   user: MiddlecatUser;
@@ -16,7 +17,7 @@ interface Props {
 
 export default function Summary({ user, indexId, query }: Props) {
   const { data: fields, isLoading: fieldsLoading } = useFields(user, indexId);
-
+  const isWriter = useHasIndexRole(user, indexId, "WRITER");
   function renderVisualization(field: AmcatField) {
     if (!field.client_settings.inListSummary) return null;
     if (field.type === "date")
@@ -24,16 +25,21 @@ export default function Summary({ user, indexId, query }: Props) {
     if (field.type === "keyword")
       return <KeywordSummaryGraph key={field.name} user={user} indexId={indexId} query={query} field={field} />;
   }
-
+  const visualizations = (fields || []).map(renderVisualization).filter((el) => el != null);
+  console.log(visualizations);
   return (
     <div className="grid snap-x snap-mandatory grid-cols-[100%,100%] gap-1 overflow-auto md:grid-cols-2 md:gap-3 md:overflow-visible">
       <div className="border-foreground/31 snap-center overflow-auto  rounded-l">
         <Articles user={user} indexId={indexId} query={query} />
       </div>
       <div className="mt-12 flex snap-center flex-col  gap-3 md:gap-6">
-        {(fields || []).map((field) => {
-          return <div key={field.name}>{renderVisualization(field)}</div>;
-        })}
+        {visualizations.length === 0 ? (
+          <div className="w-full flex-auto px-10 py-14 text-xl text-primary">
+            {isWriter ? <a href="./settings">Enable visualizations in field settings</a> : null}
+          </div>
+        ) : (
+          visualizations
+        )}
       </div>
     </div>
   );
